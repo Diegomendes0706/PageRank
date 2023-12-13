fcaf811375e687fb2d7a2509e1fa0e5a7437a866
Diegomendes0706 <diego.mendesgarcia@gmail.com>
2023-12-13 11:29:54 -0300

    any name

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def pagerank(grafo, d, M, i):
     return pr
 
 
-grafo = {'1': ["4"], '2': ['1', '3'], '3': ['4'], '4': ['2']}
+grafo = {'youtube': ["4"], '2': ['youtube', '3'], '3': ['4'], '4': ['2']}
 G = nx.DiGraph(grafo)
 
 m = nx.linalg.graphmatrix.adjacency_matrix(G).todense()  # pega a matriz que marca com 1 as conexoes
@@ -25,4 +25,4 @@ M = np.array(m).T  # faz a matriz transposta
 
 interacoes = int(input("quantas interções? "))
 for i in grafo:
-    print(f"{i} - pagerank: {pagerank(grafo, 0.85, M, interacoes)[int(i)-1][0]} ")
+    print(f"{i} - pagerank: {pagerank(grafo, 0.85, M, interacoes)[list(grafo.keys()).index(i)][0]}")
